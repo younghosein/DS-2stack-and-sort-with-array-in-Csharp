f9aa41e83a4bf09491e3acad952fb02d448384cd
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-22 01:27:40 +0330

    Add files via upload

---
+++ Two_Stack_Sort_Array/Two_Stack_Sort_Array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Stack_Sort_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoStack S = new TwoStack(100);
+
+            S.push(13);
+            S.push(93);
+            S.push(65);
+            S.push(28);
+            S.push(71);
+            S.push(92);
+            S.push(11);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Stack Is : ");
+            S.print();
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            S.pop();
+            S.pop();
+            S.bubbleSortStack(S.s, S.top1 + 1);
+            Console.WriteLine("\nAfter Sorting.....");
+            Console.WriteLine("\n\nNow Stack Is : ");
+            S.print();
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Two_Stack_Sort_Array/Two_Stack_Sort_Array/TwoStack.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Stack_Sort_Array
+{
+    class TwoStack
+    {
+        public int[] s;
+        public int top1, top2;
+        public int max;
+        public TwoStack(int size)
+        {
+            s = new int[size];
+            top1 = -1;
+            top2 = 0;
+            max = size;
+        }
+        public void print()
+        {
+            if (top2 == 0&&top1==-1)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i <= top1; i++)
+                {
+                    Console.Write(s[i] + " . ");
+                }
+            }
+        }
+        public void push(int data)
+        {
+            if (top1 == top2)
+            {
+                Console.WriteLine("Stack Is Full !");
+                return;
+            }
+            else
+            {
+                s[++top1] = data;
+                top2++;
+            }
+        }
+        public void push2(int data)
+        {
+            if (top2 == max - 1)
+            {
+                Console.WriteLine("Stack Is Full !");
+                return;
+            }
+            else
+            {
+                s[++top2] = data;
+            }
+        }
+        public int pop()
+        {
+            if (top2 == 0 && top1 == -1)
+            {
+                Console.WriteLine("Stack is Empty !");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine($"{s[top1]} Is Deleted...");
+                return s[top1--];
+            }
+        }
+        public int pop2()
+        {
+            if (top2 == 0 && top1 == -1)
+            {
+                Console.WriteLine("Stack is Empty !");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine($"{s[top2]} Is Deleted...");
+                return s[top2--];
+            }
+        }
+        public void bubbleSortStack(int[] arr, int n)
+        {
+            Stack<int> s1 = new Stack<int>();
+
+            foreach (int num in arr)
+                s1.Push(num);
+
+            Stack<int> s2 = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    while (s1.Count != 0)
+                    {
+                        int t = s1.Pop();
+
+                        if (s2.Count == 0)
+                            s2.Push(t);
+                        else
+                        {
+                            if (s2.Peek() > t)
+                            {
+                                int temp = s2.Pop();
+                                s2.Push(t);
+                                s2.Push(temp);
+                            }
+                            else
+                            {
+                                s2.Push(t);
+                            }
+                        }
+                    }
+
+                    arr[n - 1 - i] = s2.Pop();
+                }
+                else
+                {
+                    while (s2.Count != 0)
+                    {
+                        int t = s2.Pop();
+
+                        if (s1.Count == 0)
+                            s1.Push(t);
+
+                        else
+                        {
+                            if (s1.Peek() > t)
+                            {
+                                int temp = s1.Pop();
+
+                                s1.Push(t);
+                                s1.Push(temp);
+                            }
+                            else
+                                s1.Push(t);
+                        }
+                    }
+
+                    arr[n - 1 - i] = s1.Pop();
+                }
+            }
+        }
+
+    }
+}
